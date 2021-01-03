b777c2b34560d021041bd7860b07018660655088
blue-hope <ms7045436@likelion.org>
2021-01-04 03:24:43 +0900

    add swagger

--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -1,14 +1,24 @@
 import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiCreatedResponse,
+  ApiBadRequestResponse,
+} from '@nestjs/swagger';
 import { UserService } from '@app/user/user.service';
 import { createUserDto } from '@type/user/user.dto';
 import { generalUserResponse } from '@type/user/user.resp';
 import { QueryFailedError } from 'typeorm';
 
+@ApiTags('user')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @ApiCreatedResponse({
+    type: generalUserResponse,
+  })
+  @ApiBadRequestResponse()
   async create(
     @Body() createUserDto: createUserDto,
   ): Promise<generalUserResponse> {
