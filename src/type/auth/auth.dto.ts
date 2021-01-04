b8a5adfd94d8a8534f51ca12c3a38df1f266ba4f
Blue-Hope <38323921+Blue-Hope@users.noreply.github.com>
2021-01-04 19:19:17 +0900

    add jwt guard (#7)

--- src/type/auth/auth.dto.ts
+++ src/type/auth/auth.dto.ts
@@ -1,5 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { User } from '@app/user/user.entity';
+
+export class jwtPayloadDto {
+  sub: number;
+  email: string;
+}
+
 export class createAuthDto {
   @ApiProperty()
   password: string;
